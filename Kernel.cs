7bb829daf2aaf9db8b138f6821847c2a941368d6
donut2008 <76439683+donut2008@users.noreply.github.com>
2022-02-06 22:46:34 +0530

    1.11

--- Kernel.cs
+++ Kernel.cs
@@ -9,26 +9,52 @@ namespace DonutOS
     {
         protected override void BeforeRun()
         {
-            Console.WriteLine("DonutOS version 1.9");
+            Console.WriteLine("DonutOS version 1.11");
         }
 
         protected override void Run()
         {
-            Console.Write("Enter any text to print: ");
+            Console.Clear();
+            Console.WriteLine("Enter a username for this session: ");
+            var user = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("DonutOS version 1 developer build 11\n\nRunning as user "+user);
+            Console.WriteLine(user+"> ");
             var input = Console.ReadLine();
             switch(input.ToLower())
             {
                 case "about":
                     {
-                        Console.WriteLine("DonutOS version 1 developer beta build 9.");
+                        Console.WriteLine("DonutOS version 1 developer beta build 11");
                         Console.WriteLine("Built by donut2008 using CosmosOS and written in C#.");
                         break;
                     }
                 case "help":
                     {
                         Console.WriteLine("Available commands are:");
-                        Console.WriteLine("help  - Shows this message");
-                        Console.WriteLine("about - Shows information about the OS");
+                        Console.WriteLine("help   - Shows this message");
+                        Console.WriteLine("about  - Shows information about the OS");
+                        Console.WriteLine("exit   - Shuts down the OS");
+                        Console.WriteLine("reboot - Restarts the OS");
+                        Console.WriteLine("whoami - Shows current user");
+                        break;
+                    }
+                case "exit":
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Shutting down...");
+                        Cosmos.System.Power.Shutdown();
+                        break;
+                    }
+                case "reboot":
+                    {
+                        Console.WriteLine("Rebooting...");
+                        Cosmos.System.Power.Reboot();
+                        break;
+                    }
+                case "whoami":
+                    {
+                        Console.WriteLine("DonutOS\\"+user);
                         break;
                     }
                 default:
@@ -38,6 +64,10 @@ namespace DonutOS
                         break;
                     }
             }
+        }
+        private void CommandHandler(string input)
+        {
+
         }
     }
 }
