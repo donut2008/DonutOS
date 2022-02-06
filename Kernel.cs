5b262ac0a9af2b3622622e5d174d8129b57cc18b
donut2008 <76439683+donut2008@users.noreply.github.com>
2022-02-06 21:33:26 +0530

    1.7

--- Kernel.cs
+++ Kernel.cs
@@ -9,13 +9,26 @@ namespace DonutOS
     {
         protected override void BeforeRun()
         {
-            Console.WriteLine("DonutOS version 1.0");
+            Console.WriteLine("DonutOS version 1.7");
         }
 
         protected override void Run()
         {
             Console.Write("Enter any text to print: ");
             var input = Console.ReadLine();
+            switch(input.ToLower())
+            {
+                case "about":
+                    {
+                        Console.WriteLine("DonutOS version 1 developer beta build 7.");
+                        Console.WriteLine("Built by donut2008 using CosmosOS and written in C#.");
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+            }
             Console.Write("Text typed: ");
             Console.WriteLine(input);
         }
